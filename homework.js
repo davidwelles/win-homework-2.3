f652ab12462bde1342bfd81965623098fd8b5753
David Welles <74557848+davidwelles@users.noreply.github.com>
2021-02-24 15:49:42 -0500

    Add files via upload

---
+++ homework.js
@@ -0,0 +1,30 @@
+console.log('hello world');
+//homework
+let cups = 3;
+
+if( cups < 3){
+    console.log("Yes I'll take another cup of coffee.");
+} else {
+    console.log("I think I'm okay for now.");
+}
+
+let carmiles = 600;
+
+if(carmiles > 500){
+    console.log("Yes I think I need an oil change.");
+}else{
+    console.log("My car doesn't need an oil change yet.");
+}
+
+//challenge2
+let temp = 60;
+let precip = false
+let indoors = true
+if(temp > 80 && precip===false){
+    console.log ("time to swim");
+}else if(indoors) {
+    console.log ("time to swim");
+}else{
+    console.log ("The temperature is " + temp);
+}
+
